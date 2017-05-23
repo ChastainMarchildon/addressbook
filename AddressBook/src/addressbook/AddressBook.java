675e76db2e8d528dcca35114c1b422eaae61883f
ChastainMarchildon <chastainrgm@gmail.com>
2017-05-23 14:47:05 -0400

    Update AddressBook.java
    Finished the search/delete features
    Created new class methods to reduce repetition

--- AddressBook/src/addressbook/AddressBook.java
+++ AddressBook/src/addressbook/AddressBook.java
@@ -1,7 +1,4 @@
 package addressbook;
-
-import java.util.Arrays;
-import java.util.InputMismatchException;
 import javax.swing.JOptionPane;
 import java.util.Scanner;
 
@@ -26,72 +23,110 @@ public class AddressBook {
         boolean validInput = false;
         int choice;
         int adder = 1;
+
         addContact add = new addContact();
         ContactList clist = new ContactList();
-        JOptionPane dialog = new JOptionPane();
-        JOptionPane prompt = new JOptionPane();
-        JOptionPane.showMessageDialog(null, "Select an option: \n 1 - Add Contact \n 2- Delete Contact \n 3- Search Contacts \n 4- Display all contacts \n 0 - Exit");
-        System.out.println("Make a selection");
-        choice = input.nextInt();
-        /**
-         * if(input.hasNextInt()){ choice=input.nextInt(); validInput=true;
-         * }//end of if
-         *
-         */
+        choice = Integer.parseInt(JOptionPane.showInputDialog(null, "1 - Add Contact \n 2- Delete Contact \n 3- Search Contacts \n 4- Display all contacts \n 0 - Exit", "Make A Selection3", 1));
+    
+        if(choice>=0&&choice<5){ 
         while (!done) {
 
-            if (choice == 1) {
-                //add a contact
-                //Gets user input and stores it into an Array list of contact info.
-                //Increments based on how many contacts the user wants to add
-                for (int i = 0; i < adder; i++) {
-                    add.setName(JOptionPane.showInputDialog(null, "Enter a contact name", "Add Contact", 1));
-                    clist.storeName(add.getName());
+            switch (choice) {
+                case 1:
+                    //add a contact
+                    //Gets user input and stores it into an Array list of contact info.
+                    //Increments based on how many contacts the user wants to add
+                    for (int i = 0; i < adder; i++) {
+                        add.setName(JOptionPane.showInputDialog(null, "Enter a contact name", "Add Contact", 1));
+                        clist.storeName(add.getName().toUpperCase());
 
-                    add.setNumber(JOptionPane.showInputDialog(null, "Enter a contact phone number", "Add Contact", 1));
-                    clist.storeNumber(add.getNumber());
+                        add.setNumber(JOptionPane.showInputDialog(null, "Enter a contact phone number", "Add Contact", 1));
+                        clist.storeNumber(add.getNumber());
 
-                    add.setAddress(JOptionPane.showInputDialog(null, "Enter a contact Address", "Add Contact", 1));
-                    clist.storeAddress(add.getAddress());
+                        add.setAddress(JOptionPane.showInputDialog(null, "Enter a contact Address", "Add Contact", 1));
+                        clist.storeAddress(add.getAddress().toUpperCase());
 
-                    int reply = JOptionPane.showConfirmDialog(null, "Add another Contact?", "Add another Contact", 1);
-                    if (reply == JOptionPane.YES_OPTION) {
-                        adder++;
-                    }//end of if
+                        int reply = JOptionPane.showConfirmDialog(null, "Add another Contact?", "Add another Contact", 1);
+                        if (reply == JOptionPane.YES_OPTION) {
+                            adder++;
+                        }//end of if
+                        else {
+                            choice = Integer.parseInt(JOptionPane.showInputDialog(null, "1 - Add Contact \n 2- Delete Contact \n 3- Search Contacts \n 4- Display all contacts \n 0 - Exit", "Make A Selection3", 1));
+                        }
+                    }//end of for
+                    break;
+
+                case 2: {
+                    //delete a contact
+                    //Currently developing Swing skills in order to add a back/home button
+                    //Checks through each array and and returns the index for the inputted data, uses that index to delete all related info to that contact
+                    String delete = JOptionPane.showInputDialog(null, "Delete Contact", "Enter info", 1).toUpperCase();
+                    int index;
+                    if (clist.names.contains(delete)) {
+                        index = clist.names.indexOf(delete);
+                        clist.deleteContact(index, clist);
+
+                    }//of if
+                    else if (clist.numbers.contains(delete)) {
+                        index = clist.numbers.indexOf(delete);
+                        clist.deleteContact(index, clist);
+                    }//else if
+                    else if (clist.addresses.contains(delete)) {
+                        index = clist.addresses.indexOf(delete);
+                        clist.deleteContact(index, clist);
+                    }//else if
                     else{
-                        JOptionPane.showMessageDialog(null, "Select an option: \n 1 - Add Contact \n 2- Delete Contact \n 3- Search Contacts \n 4- Display all contacts \n 0 - Exit");
-                        System.out.println("Make a selection");
-                        choice=input.nextInt();
+                        JOptionPane.showMessageDialog(null,"Contact not found, returning to menu");
+                        choice = Integer.parseInt(JOptionPane.showInputDialog(null, "1 - Add Contact \n 2- Delete Contact \n 3- Search Contacts \n 4- Display all contacts \n 0 - Exit", "Make A Selection3", 1));
                     }
-                }//end of for
-                
+                }//of case 2
+                break;
+                case 3: {
+                    //search contacts
+                    //Enter contact name, number or address and return all related info
+                    //Brute force approach, not sure how else to approach this
+                    int index;
+                    //Currently developing Swing skills in order to add a back/home button
+                    String search = JOptionPane.showInputDialog(null, "Search Contacts", "Enter info", 1).toUpperCase();
+                    if (clist.names.contains(search)) {
+                        index = clist.names.indexOf(search);
+                        clist.searchContact(index, clist);
 
-            } else if (choice == 2) {
-                //delete a contact
-            } else if (choice == 3) {
-                //search contacts
-               String search= JOptionPane.showInputDialog(null, "Search Contacts", "Enter info", 1);
-                if(clist.names.contains(search)){
-                    int index=clist.names.indexOf(search);
-                    System.out.println(clist.names.get(index));
-                    System.out.println(clist.numbers.get(index));
-                    System.out.println(clist.addresses.get(index));
-                }
-                else{
-                    JOptionPane.showMessageDialog(null, "404", "Contact not found!", 0);
-                }
-                done=true;
-            } else if (choice == 4) {
-                //display all contacts*/
-                System.out.println(clist.names);
-                System.out.println(clist.numbers);
-                System.out.println(clist.addresses);
-                done=true;
-            } else if (choice == 0) {
-                System.exit(0);
-            }
+                    }//of if
+                    else if (clist.numbers.contains(search)) {
+                        index = clist.numbers.indexOf(search);
+                        clist.searchContact(index, clist);
+                    }//else if
+                    else if (clist.addresses.contains(search)) {
+                        index = clist.addresses.indexOf(search);
+                        clist.searchContact(index, clist);
+                    }//else if
+                    else {
+                        JOptionPane.showMessageDialog(null, "404", "Contact not found!", 0);
+                        choice = Integer.parseInt(JOptionPane.showInputDialog(null, "1 - Add Contact \n 2- Delete Contact \n 3- Search Contacts \n 4- Display all contacts \n 0 - Exit", "Make A Selection3", 1));
+                    }//of else
+                    
+                    break;
+                }//of case 3
 
-        }//end of while
+                case 4: {
+                    //display all contacts*/
+                    System.out.println(clist.names);
+                    System.out.println(clist.numbers);
+                    System.out.println(clist.addresses);
+                    //done = true;
+                    choice = Integer.parseInt(JOptionPane.showInputDialog(null, "1 - Add Contact \n 2- Delete Contact \n 3- Search Contacts \n 4- Display all contacts \n 0 - Exit", "Make A Selection3", 1));
+                    break;
+                }//of case 4
+                case 5: {
+                    System.exit(0);
+                    done=true;
+                    break;
+                }//of case 5
+
+            }//end of while
+        }//of switch
+    }//of input validation
 
     }//end of main
 
